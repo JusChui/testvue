71767e7ee0d2f6587c187424a8adfe67a00bc7e8
JusChui <1473441767@qq.com>
2021-04-07 18:15:14 +0800

    login and logout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,323 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <el-container>
+    <el-header style="width: 100%">
+      <span class="title" style="width: 85%">基于web的作业管理平台</span>
+      <div style="width: 15%;float: right" v-show="!isLogin">
+        <el-link type="primary" :underline="false" @click="toLoginPage">登录</el-link>&nbsp;&nbsp;
+        <el-link type="primary" :underline="false" @click="toRegisterPage">注册</el-link>
+      </div>
+      <div style="width: 18%;float: right" v-show="isLogin">
+        <i class="el-icon-user-solid">{{ usr.name }}{{ usr.status }}</i>
+        <el-button icon="el-icon-remove" style="background-color: #B3C0D1;border: none;" @click="loginOut">注销
+        </el-button>
+      </div>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <!--Aside-->
+        <el-row class="tac">
+          <el-col :span="12">
+            <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              unique-opened="unique-opened"
+              @open="handleOpen"
+              @close="handleClose"
+              @select="handleSelect">
+              <el-submenu index="1">
+                <template slot="title">
+                  <i class="el-icon-location"></i>
+                  <span>导航一</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="选项1-1">选项1-1</el-menu-item>
+                  <el-menu-item index="选项1-2">选项1-2</el-menu-item>
+                  <el-menu-item index="选项1-3">选项1-3</el-menu-item>
+                  <el-menu-item index="选项1-4">选项1-4</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-submenu index="2">
+                <template slot="title">
+                  <i class="el-icon-menu"></i>
+                  <span>导航二</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="2-1">选项2-1</el-menu-item>
+                  <el-menu-item index="2-2">选项2-2</el-menu-item>
+                  <el-menu-item index="2-3">选项2-3</el-menu-item>
+                  <el-menu-item index="2-4">选项2-4</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-submenu index="3">
+                <template slot="title">
+                  <i class="el-icon-document"></i>
+                  <span>导航三</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="3-1">选项3-1</el-menu-item>
+                  <el-menu-item index="3-2">选项3-2</el-menu-item>
+                  <el-menu-item index="3-3">选项3-3</el-menu-item>
+                  <el-menu-item index="3-4">选项3-4</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+              <el-submenu index="4">
+                <template slot="title">
+                  <i class="el-icon-setting"></i>
+                  <span slot="title">导航四</span>
+                </template>
+                <el-menu-item-group>
+                  <el-menu-item index="4-1">选项4-1</el-menu-item>
+                  <el-menu-item index="4-2">选项4-2</el-menu-item>
+                </el-menu-item-group>
+              </el-submenu>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
+      <el-main>
+        <el-row style="line-height: 0;height: auto;float: left;padding: 0.5%;background: #C0C4CC;width: 100%"
+                v-show="includeTag">
+          <el-tag
+            :key="tag.name"
+            v-for="tag in dynamicTags"
+            closable
+            :disable-transitions="false"
+            @close="closeTag(tag)"
+            @click="clickTag(tag)"
+            :type="tag.type"
+            effect="dark">
+            {{ tag.name }}
+          </el-tag>
+        </el-row>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  inject: ['reload'],
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      dynamicTags: [],
+      inputVisible: false,
+      inputValue: '',
+      includeTag: true,
+      isLogin: sessionStorage.getItem('token') === null ? false : true,
+      usr: {
+        name: '',
+        status: ''
+      }
+    }
+  },
+  methods: {
+    clickTag(tag) {
+      //点击标签事件
+      // console.log(tag)
+      let index = this.dynamicTags.indexOf(tag)
+      for (let i = 0; i < this.dynamicTags.length; i++) {
+        this.dynamicTags[i].type = 'info'
+      }
+      this.dynamicTags[index].type = ''
+    },
+    closeTag(tag) {
+      // 关闭标签
+      let index = this.dynamicTags.indexOf(tag)
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+      if (this.dynamicTags.length <= 0) {
+        this.includeTag = false
+      }
+      if (this.isIncludeActive() === false && this.dynamicTags.length > 0) {
+        this.dynamicTags[index - 1].type = ''
+      }
+    },
+    handleOpen(key, keyPath) {
+      // console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      // console.log(key, keyPath);
+    },
+    handleSelect(key, keyPath) {
+      // console.log(key, keyPath);
+      this.inputValue = keyPath[1]
+      this.handleInputConfirm()
+      this.$router.push({path: '/test'})
+    },
+    handleInputConfirm() {
+      //增加新标签
+      let inputValue = this.inputValue;
+      let tagNames = []
+      for (let i = 0; i < this.dynamicTags.length; i++) {
+        tagNames.push(this.dynamicTags[i].name)
+      }
+      //保持标签唯一
+      if (tagNames.indexOf(inputValue) < 0) {
+        for (let i = 0; i < this.dynamicTags.length; i++) {
+          this.dynamicTags[i].type = 'info'
+        }
+        this.dynamicTags.push({name: inputValue, type: ''});
+        this.includeTag = true
+      } else {
+        for (let i = 0; i < this.dynamicTags.length; i++) {
+          if (this.dynamicTags[i].name === inputValue) {
+            this.dynamicTags[i].type = ''
+          } else {
+            this.dynamicTags[i].type = 'info'
+          }
+        }
+      }
+      this.inputVisible = false;
+      this.inputValue = '';
+    },
+    isIncludeActive() {
+      //检查是否有激活状态的标签
+      if (this.dynamicTags.length > 0) {
+        for (let i = 0; i < this.dynamicTags.length; i++) {
+          if (this.dynamicTags[i].type === '') {
+            return true;
+          }
+        }
+      }
+      return false;
+    },
+    toLoginPage() {
+      //跳转登录页面
+      this.$router.push({path: '/login'})
+    },
+    toRegisterPage() {
+      //跳转注册页面
+      this.$router.push({path: '/register'})
+    },
+    updateMessage: async function () {
+      this.isLogin = false
+      this.usr.name = ''
+      this.usr.status = ''
+      sessionStorage.removeItem('token')
+      sessionStorage.removeItem('usr')
+      // console.log(this.isLogin)
+      await this.$nextTick()
+      console.log(this.$data) // => '已更新'
+    },
+    loginOut() {
+      let that = this
+      this.$axios.get('/logout', {headers: {'Authorization': sessionStorage.getItem('token')}})
+        .then((response) => {
+          console.log(response)
+          if (response.status === 200) {
+            if (response.data.rtCode === 200) {
+              that.$message({
+                message: '退出成功',
+                type: 'success'
+              });
+              this.updateMessage()
+            } else {
+              that.$message({
+                message: response.data.rtMsg,
+                type: 'warning'
+              })
+            }
+          }
+        }).catch(function (error) {
+        console.log(error);
+      });
+    }
+  },
+  mounted() {
+    if (this.dynamicTags.length <= 0) {
+      this.includeTag = false
+    }
+    let usr = JSON.parse(sessionStorage.getItem('usr'))
+    if (usr != undefined && usr != '' && usr != null) {
+      this.usr.name = usr.username
+      if (usr.status === 0) {
+        this.usr.status = '老师'
+      } else if (usr.status === 1) {
+        this.usr.status = '同学'
+      } else {
+        this.usr.status = ''
+      }
     }
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
+<style>
+body {
+  margin: 0;
 }
-ul {
-  list-style-type: none;
+
+.el-tag {
+  float: left;
+}
+
+.el-tag + .el-tag {
+  margin-left: 1px;
+}
+
+.button-new-tag {
+  margin-left: 10px;
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+
+.input-new-tag {
+  width: 90px;
+  margin-left: 10px;
+  vertical-align: bottom;
+}
+
+.el-menu {
+  background-color: #D3DCE6;
+}
+
+.el-col-12 {
+  width: 100%;
+}
+
+.el-header, .el-footer {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: left;
+  line-height: 80px;
+  height: 80px !important;
+}
+
+.title {
+  font-size: xxx-large;
+  font-family: cursive;
+}
+
+.el-header {
+  padding-left: 40px;
+}
+
+.el-aside {
+  background-color: #D3DCE6;
+  color: #333;
+  text-align: center;
+  /*line-height: 200px;*/
+  width: 18%;
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
 }
-a {
-  color: #42b983;
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
---
+++ src/views/Test.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <el-form ref="loginForm" :model="form" :rules="rules" label-width="80px" class="login-box">
+      <h3 class="login-title">欢迎登录！</h3>
+      <el-form-item label="账号">
+        <el-input type="text" placeholder="请输入账号/邮箱" v-model="form.username"/>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" placeholder="请输入密码" v-model="form.password"/>
+      </el-form-item>
+      <!--      <el-form-item label="身份">
+              <el-radio v-model="form.status" label="teacher">教师</el-radio>
+              <el-radio v-model="form.status" label="student">学生</el-radio>
+            </el-form-item>-->
+      <el-form-item>
+        <el-button v-on:click="resetForm('form')">重置</el-button>
+        <el-button type="primary" v-on:click="onSubmit('form')">登录</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-link type="primary" :underline="false" @click="toRegister">没有账号？去注册</el-link>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      form: {
+        username: '',
+        password: '',
+      }, // 表单验证，需要在 el-form-item 元素中增加 prop 属性
+      rules: {
+        username: [
+          {required: true, message: '账号不可为空', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '密码不可为空', trigger: 'blur'}
+        ],
+        status: [
+          {required: true, message: '请选择身份信息', trigger: 'blur'}
+        ]
+      },
+    }
+  },
+  methods: {
+    onSubmit() {
+      // 为表单绑定验证功能
+      let that = this
+      if (this.checkForm()) {
+        this.$axios.post('/auth/login', this.form)
+          .then(function (response) {
+            console.log(response);
+            if (response.status === 200) {
+              if (response.data.rtCode === 200) {
+                that.$message({
+                  message: '登录成功',
+                  type: 'success'
+                });
+                sessionStorage.setItem('token', response.data.bean.token)
+                sessionStorage.setItem('usr', JSON.stringify(response.data.data))
+                that.$router.push("/")
+              } else {
+                that.$message({
+                  message: response.data.returnMessage,
+                  type: 'warning'
+                })
+              }
+            }
+          }).catch(function (error) {
+          console.log(error);
+        });
+      }
+    },
+    checkForm() {
+      if (this.form.username === '') {
+        this.$message('请输入账号');
+        return false;
+      } else if (this.form.password === '') {
+        this.$message('请输入密码')
+        return false;
+      } else if (this.form.status === '') {
+        this.$message('请选择登录身份信息')
+        return false;
+      } else {
+        return true;
+      }
+    },
+    resetForm() {
+      for (let formKey in this.form) {
+        this.form[formKey] = ''
+      }
+    },
+    toRegister() {
+      this.$router.push({path: "/register"})
+    }
+  }
+}
+</script>
+
+<style>
+.login-box {
+  border: 1px solid #DCDFE6;
+  width: 350px;
+  margin: 180px auto;
+  padding: 35px 35px 15px 35px;
+  border-radius: 5px;
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  box-shadow: 0 0 25px #909399;
+}
+
+.login-title {
+  text-align: center;
+  margin: 0 auto 40px auto;
+  color: #303133;
+}
+
+.el-form-item__content {
+  text-align: end;
+  line-height: 0;
+}
+
+.el-radio {
+  margin-top: 14px;
+}
+</style>
